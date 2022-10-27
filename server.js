83d881ace1736069945eadf0f1bbe5e7c812494f
Jacob Adelman <jacobzadelman@gmail.com>
2022-10-27 17:39:52 -0400

    fix proxy settings

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 4001;
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(fileupload());
 
 
 // const corsOptions = {
@@ -35,21 +32,24 @@ var corsOptions = {
   credentials: true
 }
 app.use(cors(corsOptions))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(fileupload());
 app.use(routes);
 
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (
-    !req.secure &&
-    req.get("x-forwarded-proto") !== "https" &&
-    process.env.NODE_ENV !== "development"
-  ) {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
-app.use(requireHTTPS);
+// function requireHTTPS(req, res, next) {
+//   // The 'x-forwarded-proto' check is for Heroku
+//   if (
+//     !req.secure &&
+//     req.get("x-forwarded-proto") !== "https" &&
+//     process.env.NODE_ENV !== "development"
+//   ) {
+//     return res.redirect("https://" + req.get("host") + req.url);
+//   }
+//   next();
+// }
+// app.use(requireHTTPS);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
